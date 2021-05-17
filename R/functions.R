665765c36dd3ade3f790395552a8547d854114e8
Ari Dyckovsky <aridyckovsky@gmail.com>
2021-05-17 11:22:38 -0600

    specify config and sourcing in R Notebook

---
+++ R/functions.R
@@ -0,0 +1,3 @@
+# Your functions can go here. Then, when you want to call those functions, run
+# `source('R/functions.R')` within a code block in the RMarkdown notebook.
+print("Your scripts and functions should be in the R folder.")
